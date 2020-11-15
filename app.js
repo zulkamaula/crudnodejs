51c6cabe2008e347ac181217fc0a0c36f5e42a21
zulkamaula <mzulkariski@gmail.com>
2020-11-15 23:40:33 +0700

    membuat & menambahkan file-file pembuatan CRUD dengan node.js

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+const db = require("./config/db");
+const User = require("./models/User");
+
+app.get("/", (req,res) => res.send("respon nodejs berhasil"));
+
+db.authenticate().then(() =>
+    console.log("berhasil terkoneksi dengan database")
+);
+
+const User = require("./models/User");
+
+app.post("/crud", async (req,res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        const newUser = new User({
+            username,
+            email,
+            password
+        });
+
+        await newUser.save();
+
+        res.json(newUser);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("server error");
+    }
+});
+
+app.listen(5000, () => console.log("port berjalan di 4500")); 
